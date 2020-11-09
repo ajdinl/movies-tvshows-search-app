20b239dd403c12bc4aa36027a412ca666dbeaa9e
ajdinl <ajdinl@live.com>
2020-11-09 11:06:29 +0100

    Site design responsiveness fixed and search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,26 +3,20 @@ import React, { useState } from 'react'
 const Search = (props) => {
   const [searchValue, setSearchValue] = useState('')
 
-  const handleSearchInputChange = (e) => {
-    setSearchValue(e.target.value)
-  }
-  const resetInputField = () => {
-    setSearchValue('')
-  }
   const callSubmitFunction = (e) => {
-    e.preventDefault()
-    props.search(searchValue)
-    resetInputField()
+    setSearchValue(e.target.value)
+    setTimeout(() => {
+      props.search(searchValue)
+    }, 1000)
   }
 
   return (
-    <form className='search' onSubmit={callSubmitFunction}>
+    <form className='search'>
       <input
         type='text'
         placeholder='Search here'
-        value={searchValue}
         minLength='3'
-        onChange={handleSearchInputChange}
+        onKeyUp={callSubmitFunction}
       />
     </form>
   )
